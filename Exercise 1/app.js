04c5569bb653eaeb8c868a974af33e9be2174c09
yac0819 <yac000192@gmail.com>
2020-02-22 17:58:37 +0900

    exerciese one

---
+++ Exercise 1/app.js
@@ -0,0 +1,16 @@
+new Vue({
+	el: '#exercise',
+  data: {
+  	name: 'Annie',
+    age: 22,
+    imageLink:'https://cdn.getstickerpack.com/storage/uploads/sticker-pack/ice-bear/sticker_1.png?afc541aae29090d288eebe018a8f726c',
+    
+  
+  },
+  methods: {
+  	randomFloat: function() {
+    	return Math.random();
+    },
+  
+  }
+})
