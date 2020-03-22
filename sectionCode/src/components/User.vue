144991bc1997ea49955789a072431291dba81c5c
yac0819 <yac000192@gmail.com>
2020-03-22 22:30:28 +0900

    component communication child to child

--- sectionCode/src/components/User.vue
+++ sectionCode/src/components/User.vue
@@ -6,10 +6,18 @@
         <hr>
         <div class="row">
             <div class="col-xs-12 col-sm-6">
-                <app-user-detail :name="name" @nameWasReset="name = $event"></app-user-detail>
+                <app-user-detail 
+                    :name="name" 
+                    @nameWasReset="name = $event"
+                    :resetFn="resetName"
+                    :userAge="age"
+                ></app-user-detail>
             </div>
             <div class="col-xs-12 col-sm-6">
-                <app-user-edit></app-user-edit>
+                <app-user-edit 
+                    :userAge="age"
+                    @ageWasEdited="age = $event"
+                ></app-user-edit>
             </div>
         </div>
     </div>
@@ -23,11 +31,15 @@
         data: function() {
             return {
                 name: 'Ann',
+                age: 27,
             }
         },
         methods: {
             changeName() {
                 this.name = 'Annie';
+            },
+            resetName() {
+                this.name = 'Ann'
             }
         },
         components: {
--- sectionCode/src/components/UserDetail.vue
+++ sectionCode/src/components/UserDetail.vue
@@ -3,16 +3,20 @@
         <h3>You may view the User Details here</h3>
         <p>Many Details</p>
         <p>User Name: {{ switchName() }}</p>
+        <p>User Age: {{ userAge }}</p>
         <button @click="resetName">Reset Name</button>
+        <button @click="resetFn()">Reset Name</button>
     </div>
 </template>
 
 <script>
     export default{
-        props:{
+        props: {
             name: {
                 type: String
-            }
+            },
+            resetFn: Function,
+            userAge: Number,
         },
         methods:{
             switchName() {
