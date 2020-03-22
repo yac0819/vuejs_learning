6041e888ab8baa1deff1af934c5a7955166e9a47
yac0819 <yac000192@gmail.com>
2020-03-22 15:41:25 +0900

    pass data from child to parent

--- sectionCode/src/components/UserDetail.vue
+++ sectionCode/src/components/UserDetail.vue
@@ -2,13 +2,27 @@
     <div class="component">
         <h3>You may view the User Details here</h3>
         <p>Many Details</p>
-        <p>User Name: {{ name }}</p>
+        <p>User Name: {{ switchName() }}</p>
+        <button @click="resetName">Reset Name</button>
     </div>
 </template>
 
 <script>
     export default{
-        props: ['name']
+        props:{
+            name: {
+                type: String
+            }
+        },
+        methods:{
+            switchName() {
+                return this.name.split("").reverse().join("");
+            },
+            resetName() {
+                this.name = "Ann";
+                this.$emit('nameWasReset', this.name);
+            }
+        }
     }
 </script>
 
