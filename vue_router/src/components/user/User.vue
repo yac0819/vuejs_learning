8521e2f1bcc5e32d4ad01c41bf8e343fc54758e5
yac0819 <yac000192@gmail.com>
2020-04-05 18:34:01 +0900

    route learning

---
+++ vue_router/src/components/user/User.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <h1>The User Page</h1>
+        <hr>
+        <p>Loaded ID: {{ id }}</p>
+        <button @click="navigateToHome" class="btn btn-primary">Go to Home</button>
+        <hr>
+        <router-view></router-view>
+    </div>
+</template>
+
+<script>
+    export default {
+        methods: {
+            navigateToHome() {
+                this.$router.push({name: 'home'});
+            }
+        }   
+    }
+</script>
